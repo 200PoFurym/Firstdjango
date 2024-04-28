736644b86bfb21f3ebc94d523fccda442fe0354d
Marat <maratiknyam@gmail.com>
2024-04-28 18:30:52 +0500

    django

--- newsworld/views.py
+++ newsworld/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from newsworld.models import News
+
+
 # Create your views here.
+def home_page(request):
+    news = News.objects.all()
+    context = {'news': news}
+    return render(request, template_name='index.html', context=context)
+
